399817d77aec776f1b9ad97f3026ff6c256c745d
MrZloHex <zlo.alex.it@gmail.com>
2021-10-27 12:05:51 +0300

    FIRST TEST

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ pub mod instruction;
 
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
+
+solana_program::declare_id!("4uQeVj5tqViQh7yWWGStvkEG1Zmhx6uasJtWCJziofM");
--- tests/amm.rs
+++ tests/amm.rs
@@ -1,7 +1,7 @@
 #![cfg(feature = "test-bpf")]
 
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_amm::{entrypoint::process_instruction, instruction::AmmInstruction};
+use solana_amm::{entrypoint::process_instruction, id, instruction::AmmInstruction};
 use solana_program_test::{processor, tokio, ProgramTest, ProgramTestContext};
 use solana_program::system_instruction;
 use solana_sdk::{
@@ -18,8 +18,7 @@ struct Env {
 
 impl Env {
     async fn new() -> Self {
-        let id = Pubkey::new_unique();
-        let program_test = ProgramTest::new("solana-amm", id, processor!(process_instruction));
+        let program_test = ProgramTest::new("solana_amm", id(), processor!(process_instruction));
         let mut ctx = program_test.start_with_context().await;
 
         let user = Keypair::new();
