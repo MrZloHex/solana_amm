43da6890bb360bc86ce8bca7301da44104999c2a
MrZloHex <zlo.alex.it@gmail.com>
2021-10-28 01:30:27 +0300

    MORE ABSTRACTS

--- src/instruction.rs
+++ src/instruction.rs
@@ -22,30 +22,76 @@ impl TransferAMM {
     }
 }
 
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug)]
+pub struct CreateSettlementAccounts {
+    quantity_tokens_a: u64,
+    quantity_tokens_b: u64
+}
+
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug)]
+pub enum InstructionType {
+    /// Accounts:
+    /// 0. `[]` program's account with tokens type A, PDA
+    /// 1. `[]` program's account with tokens type B, PDA
+    /// 2. `[]` user's account with tokens type A
+    /// 3. `[]` user's account with tokens type B
+    TransferAMM(TransferAMM),
+
+    /// Accounts:
+    /// 0. `[signer, writable]` payer's account, who will provide tokens for program's accounts
+    CreateSettlementAccounts(CreateSettlementAccounts)
+}
+
 #[cfg(test)]
 mod test {
     use borsh::{BorshDeserialize, BorshSerialize};
 
-    use crate::instruction::{TokenType, TransferAMM};
+    use crate::instruction::{
+        TokenType,
+        TransferAMM,
+        InstructionType,
+        CreateSettlementAccounts
+    };
 
     #[test]
-    fn test_serialization() {
-        let data = TransferAMM {
+    fn test_transfer_serialization() {
+        let data = InstructionType::TransferAMM( TransferAMM {
             token_type: TokenType::A,
             quantity: 35_000,
-        }
+        })
         .try_to_vec()
         .unwrap();
-        assert_eq!(data, [0, 184, 136, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(data, [0, 0, 184, 136, 0, 0, 0, 0, 0, 0]);
     }
 
     #[test]
-    fn test_deserialization() {
-        let data = TransferAMM {
+    fn test_transfer_deserialization() {
+        let data = InstructionType::TransferAMM( TransferAMM {
             token_type: TokenType::B,
             quantity: 35_000,
-        };
-        let instr = TransferAMM::try_from_slice(&[1, 184, 136, 0, 0, 0, 0, 0, 0]).unwrap();
+        });
+        let instr = InstructionType::try_from_slice(&[0, 1, 184, 136, 0, 0, 0, 0, 0, 0]).unwrap();
+        assert_eq!(data, instr);
+    }
+
+    #[test]
+    fn test_create_acc_serialization() {
+        let data = InstructionType::CreateSettlementAccounts( CreateSettlementAccounts {
+            quantity_tokens_a: 1234,
+            quantity_tokens_b: 4567
+        })
+        .try_to_vec()
+        .unwrap();
+        assert_eq!(data, [1, 210, 4, 0, 0, 0, 0, 0, 0, 215, 17, 0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_create_acc_deserialization() {
+        let data = InstructionType::CreateSettlementAccounts( CreateSettlementAccounts {
+            quantity_tokens_a: 7890,
+            quantity_tokens_b: 3465
+        });
+        let instr = InstructionType::try_from_slice(&[1, 210, 30, 0, 0, 0, 0, 0, 0, 137, 13, 0, 0, 0, 0, 0, 0]).unwrap();
         assert_eq!(data, instr);
     }
 }
